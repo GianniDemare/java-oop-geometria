c9c59f2f09a501ca7de760c28a99c76ce1306b29
xgiannix <gianni.demare@hotmail.it>
2023-12-05 15:09:33 +0100

    Finisced

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,33 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // CREARE UNO SCANNER PER LEGGERE INPUT DA TERMINALE
+        Scanner scan = new Scanner(System.in);
+
+        // CHIEDERE ALL'UTENTE DI INSERIRE I VALORI DI BASE E ALTEZZA
+        System.out.println("Inserisci il valore della base del rettangolo:");
+        int newBaseValue = scan.nextInt();
+        System.out.println("Inserisci il valore dell'altezza del rettangolo:");
+        int newHeightValue = scan.nextInt();
+
+        Rettangolo rettangolo = new Rettangolo();
+        rettangolo.rettangoloAdd(newBaseValue, newHeightValue);
+
+        // UTILIZZO DEI METODI PER CALCOLARE AREA E PERIMETRO
+        int area = rettangolo.calculateArea();
+        int perimetro = rettangolo.calculatePerimeter();
+
+        // STAMPA DEI RISULTATI
+        System.out.println("Area del rettangolo: " + area);
+        System.out.println("Perimetro del rettangolo: " + perimetro);
+
+        // DISEGNA IL RETTANGOLO IN CONSOLE
+        rettangolo.disegna();
+
+        scan.close();
+
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,51 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+    // ATTRIBUTI
+    int base;
+    int height;
+
+    // COSTRUTTORI
+    Rettangolo() {
+        base = 20;
+        height = 10;
+    }
+
+
+    // METODI
+    void rettangoloAdd(int baseAdd, int heightAdd){
+        base = baseAdd;
+        height = heightAdd;
+    }
+
+    // METODO PER CALCOLARE L'AREA DEL RETTANGOLO
+    public int calculateArea() {
+        return base * height;
+    }
+
+    // METODO PER CALCOLARE IL PERIMETRO DEL RETTANGOLO
+    public int calculatePerimeter() {
+        return 2 * (base + height);
+    }
+
+    // METODO PER DISEGNARE IL RETTANGOLO IN CONSOLE
+    public void disegna() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < base; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == base - 1) {
+                    // STAMPA IL BORDO DEL RETTANGOLO
+                    System.out.print("0");
+                } else {
+                    // STAMPA GLI SPAZI ALL'INTERNO DEL RETTANGOLO
+                    System.out.print(" ");
+                }
+            }
+            // VAI A CAPO ALLA FINE DI OGNI RIGA
+            System.out.println();
+        }
+    }
+
+
 }
+
+
